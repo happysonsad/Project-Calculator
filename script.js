b28a0e781a78f4e3df87fd5418b8df8dc1022dcd
happysonsad <176979803+happysonsad@users.noreply.github.com>
2024-09-03 17:37:24 +0800

    '1' button can now add to display

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let fisrtNum = "";
 let operator = "";
 let secondNum = "";
 
+let displayValue = "";
+
 const add = function add(a, b) {
     return a + b;
 }
@@ -23,3 +25,34 @@ const divide = function divide(a, b) {
 function operate(a, operator, b) {
     add(a, b)
 }
+
+const display = document.querySelector("#display");
+// display.textContent = "";
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
+one.addEventListener(("click"), () => {
+    if (display.textContent === "0") {
+        return display.textContent = "1"
+    }
+    return display.textContent += "1";
+})
+
+function getNumber(num) {
+    num.addEventListener(("click"), (num) => {
+        if (display.textContent === "0") {
+            return display.textContent = num;
+        } else if (display)
+        return display.textContent += num;
+    })
+}
+
