fd4877554fe955243986d779a33ac779245f42fb
happysonsad <176979803+happysonsad@users.noreply.github.com>
2024-09-04 11:12:26 +0800

    created functions to the display when clicked the # buttons and stored the values

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let fisrtNum = "";
 let operator = "";
 let secondNum = "";
 
-let displayValue = "";
-
 const add = function add(a, b) {
     return a + b;
 }
@@ -26,8 +24,9 @@ function operate(a, operator, b) {
     add(a, b)
 }
 
+let displayValue = 0;
 const display = document.querySelector("#display");
-// display.textContent = "";
+display.textContent = displayValue;
 
 const zero = document.querySelector("#zero");
 const one = document.querySelector("#one");
@@ -40,19 +39,34 @@ const seven = document.querySelector("#seven");
 const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
 
-one.addEventListener(("click"), () => {
-    if (display.textContent === "0") {
-        return display.textContent = "1"
-    }
-    return display.textContent += "1";
-})
+// one.addEventListener(("click"), () => {
+//     if (display.textContent === "0") {
+//         return display.textContent = one.textContent;
+//     }
+//     return display.textContent += one.textContent;
+// })
 
 function getNumber(num) {
-    num.addEventListener(("click"), (num) => {
+    num.addEventListener(("click"), () => {
         if (display.textContent === "0") {
-            return display.textContent = num;
-        } else if (display)
-        return display.textContent += num;
+            displayValue = num.textContent;
+            return display.textContent = num.textContent;
+        } else
+            displayValue += num.textContent;
+            return display.textContent += num.textContent;
     })
 }
 
+getNumber(zero);
+getNumber(one);
+getNumber(two);
+getNumber(three);
+getNumber(four);
+getNumber(five);
+getNumber(six);
+getNumber(seven);
+getNumber(eight);
+getNumber(nine);
+
+
+console.log(displayValue)
